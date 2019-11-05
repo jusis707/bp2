e1b0f935f90f795b4b7c827d4945fd7c75f844ce
jusis707 <48894008+jusis707@users.noreply.github.com>
2019-11-05 07:30:22 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,13 +13,13 @@ do
    I=$(shuf -i 1-4 -n 1)
    D=`date -Iseconds`
    case "$I" in
-      "1") echo "$D APP5 ERROR An error is usually an exception that has been caught and not handled."
+      "1") echo "$D APP3 ERROR An error is usually an exception that has been caught and not handled."
       ;;
-      "2") echo "$D APP5 INFO This is less important than debug log and is often used to provide context in the current task."
+      "2") echo "$D APP3 INFO This is less important than debug log and is often used to provide context in the current task."
       ;;
-      "3") echo "$D APP5 WARN A warning that should be ignored is usually at this level and should be actionable."
+      "3") echo "$D APP3 WARN A warning that should be ignored is usually at this level and should be actionable."
       ;;
-      "4") echo "$D APP5 DEBUG This is a debug log that shows a log that can be ignored."
+      "4") echo "$D APP3 DEBUG This is a debug log that shows a log that can be ignored."
       ;;
    esac
    c=$(( c+1 ))
